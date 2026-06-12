407cff0444110e9041a95eb2c0f51a45f8765855
Đào Tuấn Phúc <phucowo@gmail.com>
2026-06-12 10:01:09 +0700

    v1

--- DoAnKi3/Controllers/HomeController.cs
+++ DoAnKi3/Controllers/HomeController.cs
@@ -13,17 +13,14 @@ namespace DoAnKi3.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Aboutus()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ DoAnKi3/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace DoAnKi3.Controllers
+{
+    public class ProductController : Controller
+    {
+       public ActionResult shop()
+        {
+            return View();
+        }
+
+    }
+}
